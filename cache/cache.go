f4a1fadd829b3e8d34fda02b18b8938aef2b40f5
Kemal Akkoyun <kakkoyun@gmail.com>
2019-02-14 18:35:39 +0100

    WIP: Use golang tar library for reading

--- cache/cache.go
+++ cache/cache.go
@@ -3,14 +3,12 @@ package cache
 
 import (
 	"archive/tar"
-	"bytes"
 	"compress/gzip"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/pkg/errors"
@@ -53,19 +51,21 @@ func (c Cache) Upload(src, dst string) error {
 	if err != nil {
 		return errors.Wrap(err, fmt.Sprintf("could not create tarball file <%s>", archivePath))
 	}
-	tw, closer := archiveWriter(file, c.archiveFmt)
+	tw, twCloser := archiveWriter(file, c.archiveFmt)
+	closer := func() {
+		twCloser()
+		file.Close()
+	}
+	defer closer()
 
 	// 3. walk through source and add each file
 	err = filepath.Walk(src, writeFileToArchive(tw, src))
 	if err != nil {
-		closer()
-		file.Close()
 		return errors.Wrap(err, "could not add all files to archive")
 	}
 
 	// 4. Close resources before upload
 	closer()
-	file.Close()
 
 	// 5. upload archive file to server
 	log.Printf("uploading archived directory <%s> to <%s>", src, dst)
@@ -85,13 +85,15 @@ func (c Cache) uploadArchive(dst, archivePath string) error {
 // Download fetches the archived file from the cache and restores to the host machine's file system
 func (c Cache) Download(src, dst string) error {
 	log.Printf("dowloading archived directory <%s>", src)
+
+	// 1. download archive
 	rc, err := c.b.Get(src)
 	if err != nil {
 		return errors.Wrap(err, "could not get file from storage backend")
 	}
 	defer rc.Close()
 
-	// create temp file for archive
+	// 2. create temp file for archive
 	temp, err := ioutil.TempFile("", "")
 	if err != nil {
 		return errors.Wrap(err, "could not create tmp file to archive")
@@ -101,46 +103,44 @@ func (c Cache) Download(src, dst string) error {
 		os.Remove(temp.Name())
 	}()
 
-	// download archive to temp file
+	// 3. write downloaded archive to temp file
 	if _, err := io.Copy(temp, rc); err != nil {
 		errors.Wrap(err, "could not copy downloaded file to tmp")
 	}
+	rc.Close()
 
-	// run extraction command
+	// 4. extract archive
 	log.Printf("extracting archived directory <%s> to <%s>", src, dst)
-	cmd := exec.Command("tar", "-xf", temp.Name(), "-C", "/")
-	var stdout, stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	defer log.Printf("command stout: <%s>, stderr: <%s>", string(stdout.Bytes()), string(stderr.Bytes()))
+	tr := archiveReader(temp, c.archiveFmt)
+	return errors.Wrap(extractFilesFromArchive(tr, dst), "could not extract files from downloaded archive")
 
-	return errors.Wrap(cmd.Run(), "could not open extract downloaded file")
+	// // run extraction command
+	// cmd := exec.Command("tar", "-xf", temp.Name(), "-C", "/")
+	// var stdout, stderr bytes.Buffer
+	// cmd.Stdout = &stdout
+	// cmd.Stderr = &stderr
+	// defer log.Printf("command stout: <%s>, stderr: <%s>", string(stdout.Bytes()), string(stderr.Bytes()))
+
+	// return errors.Wrap(cmd.Run(), "could not open extract downloaded file")
 }
 
 // Helpers
 
 func archiveWriter(w io.Writer, archiveFmt string) (*tar.Writer, func()) {
-	var tw *tar.Writer
-	var closer func()
-	switch archiveFmt {
-	case "tar":
-		tw = tar.NewWriter(w)
-		closer = func() { tw.Close() }
-	case "gzip":
+	tw := tar.NewWriter(w)
+	closer := func() { tw.Close() }
+	if archiveFmt == "gzip" {
 		gw := gzip.NewWriter(w)
 		tw = tar.NewWriter(gw)
 		closer = func() {
 			gw.Close()
 			tw.Close()
 		}
-	default:
-		tw = tar.NewWriter(w)
-		closer = func() { tw.Close() }
 	}
 	return tw, closer
 }
 
-func writeFileToArchive(tw *tar.Writer, rootPath string) func(path string, fi os.FileInfo, err error) error {
+func writeFileToArchive(tw *tar.Writer, src string) func(path string, fi os.FileInfo, err error) error {
 	return func(path string, fi os.FileInfo, err error) error {
 		if !fi.Mode().IsRegular() { // skip on symbolic links or directories
 			return nil
@@ -169,5 +169,59 @@ func writeFileToArchive(tw *tar.Writer, rootPath string) func(path string, fi os
 		}
 
 		return nil
+	}
+}
+
+func archiveReader(r io.Reader, archiveFmt string) *tar.Reader {
+	tr := tar.NewReader(r)
+	if archiveFmt == "gzip" {
+		gzr, err := gzip.NewReader(r)
+		if err != nil {
+			gzr.Close()
+			return tr
+		}
+		return tar.NewReader(gzr)
+	}
+	return tr
+}
+
+func extractFilesFromArchive(tr *tar.Reader, dst string) error {
+	for {
+		h, err := tr.Next()
+		switch {
+		case err == io.EOF: // if no more files are found return
+			return nil
+		case err != nil: // return any other error
+			return err
+		case h == nil: // if the header is nil,skip it
+			continue
+		}
+
+		target := filepath.Join(dst, h.Name) // the target location where the dir/file should be created
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		switch h.Typeflag {
+		case tar.TypeDir: // if its a dir and it doesn't exist create it
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		case tar.TypeReg: // if it's a file create it
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(h.Mode))
+			if err != nil {
+				return err
+			}
+			if _, err := io.Copy(f, tr); err != nil {
+				f.Close()
+				return err
+			}
+
+			f.Close()
+		}
 	}
 }
