2d8d85166eb930209e13155ab239a85decdbe692
Kemal Akkoyun <kakkoyun@gmail.com>
2019-05-15 15:17:00 +0200

    Make CI happy

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -116,7 +116,7 @@ func processRebuild(c cache.Cache, cacheKeyTmpl string, mountedDirs []string, m 
 
 	for _, mount := range mountedDirs {
 		if _, err := os.Stat(mount); err != nil {
-			return errors.Wrap(err, fmt.Sprintf("could not use <%s> to cache, make sure file or directory exists and readable", mount))
+			return errors.Wrap(err, fmt.Sprintf("could not mount <%s>, make sure file or directory exists and readable", mount))
 		}
 
 		key, err := cacheKey(m, cacheKeyTmpl, mount, branch)
