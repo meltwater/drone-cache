0efa95f6fe457a2caf622bc478504b84f7d4c4c3
Harri Hämäläinen <hhamalai@iki.fi>
2020-11-05 20:23:16 +0200

    Add optional param for AWS IAM role assumption
    Upgrade minio version and add user & policy initialization
    
    Fix permissions
    
    Organize imports

--- storage/backend/s3/config.go
+++ storage/backend/s3/config.go
@@ -15,6 +15,7 @@ type Config struct {
 	Encryption string // if not "", enables server-side encryption. valid values are: AES256, aws:kms.
 	Endpoint   string
 	Key        string
+	RoleArn    string // if "", do not assume IAM role i.e. use the IAM user.
 
 	// us-east-1
 	// us-west-1
